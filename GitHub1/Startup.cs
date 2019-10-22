5560c3eb5853967ec3e8784167476aec3e44f30c
Ander Maestre <andermaestre_90@hotmail.es>
2019-10-22 08:54:44 +0200

    Tercer cambio

--- GitHub1/Startup.cs
+++ GitHub1/Startup.cs
@@ -14,6 +14,7 @@ namespace GitHub1
 
         //Mi primer cambio
         //Otro cambio mas
+        //Mi tercer cambio
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
