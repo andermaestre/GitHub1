efb4e1882a3c4f05ea3b015bfcdbe2d7f25e65e9
Ander Maestre <andermaestre_90@hotmail.es>
2019-10-22 08:44:25 +0200

    Mi primer cambio

--- GitHub1/Startup.cs
+++ GitHub1/Startup.cs
@@ -11,6 +11,8 @@ namespace GitHub1
 {
     public class Startup
     {
+
+        //Mi primer cambio
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
